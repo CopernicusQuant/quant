93fa8c54df1f36bbe6941d2e6ffa404d76c67f88
akane9506 <akane19950611@gmail.com>
2026-09-02 22:52:49 -0700

    add data download and local data loading method

--- data/data.py
+++ data/data.py
@@ -4,6 +4,7 @@ import shutil
 from pathlib import Path
 
 import pandas as pd
+import pyarrow.parquet as pq
 from pyarrow import fs
 
 from config import get_config
@@ -38,17 +39,47 @@ class DataStore:
 
         # Setup local path
         self._ensure_local_dirs()
+        self._r2_stock_list_path = (
+            f"{self.bucket_name}/{META_DIR}/{self.stock_list_filename}.csv"
+        )
+        self._r2_stocks_path = f"{self.bucket_name}/{STOCK_DIR}"
+        self._local_stock_list_path = f"{LOCAL_DATA_FOLDER}/{self._r2_stock_list_path}"
+        self._local_stocks_path = f"{LOCAL_DATA_FOLDER}/{self._r2_stocks_path}"
 
     def get_stock_list(self) -> pd.DataFrame:
-        stock_list_path = Path(self._local_stock_list_path())
+        stock_list_path = Path(self._local_stock_list_path)
         if not stock_list_path.exists():
             logger.info("downloading stock list from R2 storage...")
             with (
-                self.fs.open_input_file(self._r2_stock_list_path()) as source,
+                self.fs.open_input_file(self._r2_stock_list_path) as source,
                 stock_list_path.open("wb") as target,
             ):
                 shutil.copyfileobj(source, target)
         df = pd.read_csv(stock_list_path)
+        return df
+
+    def download_stocks(self):
+        stock_list_df = self.get_stock_list()
+        stocks = stock_list_df["ts_code"].to_list()
+        for i, stock in enumerate(stocks):
+            remote_file = f"{self._r2_stocks_path}/{stock}.parquet"
+            local_path = f"{self._local_stocks_path}/{stock}.parquet"
+            with (
+                self.fs.open_input_file(remote_file) as source,
+                open(local_path, "wb") as target,
+            ):
+                shutil.copyfileobj(source, target)
+            if (i + 1) % 10 == 0:
+                logger.info(f"{i + 1} stock files downloaded")
+        logger.info("stock download completed")
+
+    def load_stocks(self, filters: tuple | None = None) -> pd.DataFrame:
+        if filters:
+            dataset = pq.ParquetDataset(self._local_stocks_path, filters=filters)
+        else:
+            dataset = pq.ParquetDataset(self._local_stocks_path)
+        table = dataset.read()
+        df = table.to_pandas()
         return df
 
     def _ensure_local_dirs(self):
@@ -56,9 +87,3 @@ class DataStore:
             os.makedirs(
                 f"{LOCAL_DATA_FOLDER}/{self.bucket_name}/{folder}", exist_ok=True
             )
-
-    def _r2_stock_list_path(self) -> str:
-        return f"{self.bucket_name}/{META_DIR}/{self.stock_list_filename}.csv"
-
-    def _local_stock_list_path(self) -> str:
-        return f"{LOCAL_DATA_FOLDER}/{self._r2_stock_list_path()}"
