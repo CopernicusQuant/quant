271ded0a27dcbaffa656e1f0244518816987c482
akane9506 <akane19950611@gmail.com>
2026-09-03 21:45:48 -0700

    add DEMA calculator

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import logging
 
 from data import DataStore
+from feature.calculator import compute_dema
+from visualization.plot import vis_dema
 
 logging.basicConfig(level=logging.INFO)
 
 
 def main():
     store = DataStore()
-    store.load_stocks()
+    # store.load_stocks()
+    stock = store.load_single_stock(
+        ts_code="MO",
+    )
+    result = compute_dema(stock, 20, 120)
+    vis_dema(result)
 
 
 if __name__ == "__main__":
