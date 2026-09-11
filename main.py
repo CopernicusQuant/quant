5432cb560448489855812a662d978e11e33e3f4a
akane9506 <akane19950611@gmail.com>
2026-09-10 17:39:49 -0700

    add cci

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ from feature.calculator_utils import (
     # compute_price_momentum,
     # compute_volume_momentum,
     # compute_activity_features,
-    compute_volatility,
+    # compute_volatility,
+    compute_cci,
 )
 
 # from visualization.plot import (
@@ -26,7 +27,8 @@ from feature.calculator_utils import (
 # from visualization.price_momentum import vis_price_momentum
 # from visualization.volume_momentum import vis_volume_momentum
 # from visualization.activity import vis_activity_features
-from visualization.volatility import vis_volatility
+# from visualization.volatility import vis_volatility
+from visualization.cci import vis_cci
 
 logging.basicConfig(level=logging.INFO)
 
@@ -50,8 +52,11 @@ def main():
     # vis_price_momentum(stock, result[result.index >= "20250810"])
     # result = compute_volume_momentum(stock)
     # vis_volume_momentum(stock, result[result.index >= "20250910"])
-    result = compute_volatility(stock)
-    vis_volatility(stock, result)
+    # result = compute_volatility(stock)
+    # vis_volatility(stock, result)
+    result = compute_cci(stock)
+    vis_cci(stock_df=stock, cci_df=result)
+    # print(result.tail(30))
 
 
 if __name__ == "__main__":
